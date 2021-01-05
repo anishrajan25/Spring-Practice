b227a14913854b2ae42dbe8f7938966da74fb663
Anish Rajan <anishrajan2708@gmail.com>
2021-01-05 18:45:20 +0530

    created constructor and configured application context for dependency injection

--- spring-demo-one/src/com/practice/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/practice/springdemo/BaseballCoach.java
@@ -2,9 +2,24 @@ package com.practice.springdemo;
 
 public class BaseballCoach implements Coach {
 
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	// define a constructor for dependency injection
+	public BaseballCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	public BaseballCoach() {}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 	
 }
--- spring-demo-one/src/com/practice/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/practice/springdemo/TrackCoach.java
@@ -1,10 +1,25 @@
 package com.practice.springdemo;
 
 public class TrackCoach implements Coach {
-
+	
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	// define a constructor for dependency injection
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
+	public TrackCoach() {}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
