0331eee1613eb55d0af5869b1cb877c1a1de2564
Anish Rajan <anishrajan2708@gmail.com>
2021-01-06 17:47:09 +0530

    default bean id/ component names

--- spring-demo-annotations/src/com/practice/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/practice/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.practice.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
