5bc798d820a8d9f550feb838108e40d18b81ccfb
Anish Rajan <anishrajan2708@gmail.com>
2021-01-06 21:24:24 +0530

    lifecycle methods annotation

--- spring-demo-annotations/src/com/practice/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/practice/springdemo/TennisCoach.java
@@ -2,11 +2,10 @@ package com.practice.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import javax.annotation.*;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
@@ -17,6 +16,16 @@ public class TennisCoach implements Coach {
 //	public TennisCoach(FortuneService fortuneService) {
 //		this.fortuneService = fortuneService;
 //	}
+	
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println("TennisCoach inside doMyStartupStuff()");
+	}
+	
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println("TennisCoach inside doMyCleanupStuff()");
+	}
 	
 	public TennisCoach() {
 		System.out.println("TennisCoach: inside default constructor");
