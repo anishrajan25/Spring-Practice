984df30a377d5992fad5d15c55cf21390a21e5a2
Anish Rajan <anishrajan2708@gmail.com>
2021-01-06 16:40:07 +0530

    bean lifecycle

--- spring-demo-one/src/com/practice/springdemo/BeanScopeDemoApp.java
+++ spring-demo-one/src/com/practice/springdemo/BeanScopeDemoApp.java
@@ -22,6 +22,8 @@ public class BeanScopeDemoApp {
 		System.out.println("\nMemory location for theCoach: " + theCoach);
 		System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
 		
+		// close the context
+		context.close();
 	}
 
 }
--- spring-demo-one/src/com/practice/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/practice/springdemo/TrackCoach.java
@@ -22,4 +22,11 @@ public class TrackCoach implements Coach {
 		return "Just Do It! " + fortuneService.getFortune();
 	}
 
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside init function: doMyStartupStuff");
+	}
+	
+	public void doMyCleanupStuff() {
+		System.out.println("TrackCoach: inside destroy function: doMyCleanupStuff");
+	}
 }
