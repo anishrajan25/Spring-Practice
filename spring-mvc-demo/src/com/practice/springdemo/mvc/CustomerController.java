1961e4b4ef07ebd81a681d0d9e84b351ea51e9bc
Anish Rajan <anishrajan2708@gmail.com>
2021-01-12 23:08:54 +0530

    used web data binder and custom string trimmer editor to trim strings

--- spring-mvc-demo/src/com/practice/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/practice/springdemo/mvc/CustomerController.java
@@ -1,8 +1,11 @@
 package com.practice.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +14,15 @@ import javax.validation.Valid;
 @Component
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+		
+	}
 
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
