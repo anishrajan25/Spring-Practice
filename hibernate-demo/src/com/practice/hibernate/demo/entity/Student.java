7ba99647c93b590da8dc5cf32cd81e9a06da7161
Anish Rajan <anishrajan2708@gmail.com>
2021-01-21 21:26:58 +0530

    making entity class

--- hibernate-demo/src/com/practice/hibernate/demo/entity/Student.java
+++ hibernate-demo/src/com/practice/hibernate/demo/entity/Student.java
@@ -1,5 +1,72 @@
 package com.practice.hibernate.demo.entity;
 
-public class Student {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="student")
+public class Student {
+	
+	public Student() {
+	
+	}
+	
+	public Student(int firstName, int lastName, int email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private int firstName;
+
+	@Column(name="last_name")
+	private int lastName;
+
+	@Column(name="email")
+	private int email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(int firstName) {
+		this.firstName = firstName;
+	}
+
+	public int getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(int lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getEmail() {
+		return email;
+	}
+
+	public void setEmail(int email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
 }
